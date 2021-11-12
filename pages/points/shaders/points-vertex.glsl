3fd1f3c68b0d269c11efb8e1c1e54ce3b014f345
Nikolai Arsenov <nikolai.a.arsenov@hotmail.com>
2021-11-12 15:28:33 +0100

    minor shaders cleanup

--- pages/points/shaders/points-vertex.glsl
+++ pages/points/shaders/points-vertex.glsl
@@ -1,17 +1,10 @@
-precision mediump float;
-
+precision highp float;
 uniform mat4 u_camera;
 
 attribute vec3 a_position;
 
-varying float v_w;
-
 void main(void) {
-
-  vec4 finalPosition = u_camera * vec4(a_position, 1.0);
-
-  gl_Position = finalPosition;
-  v_w = 1.0 / finalPosition.w;
+  gl_Position = u_camera * vec4(a_position, 1.0);
 
   if (gl_Position.w > 0.0) {
     gl_PointSize = 4.0 / gl_Position.w;
